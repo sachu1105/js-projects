1b1e3e79d3dc5d996317ce9919d3bb99252f652b
sachu <sachusajith170@gmail.com>
2024-06-30 10:53:36 +0530

    project one completed

--- ProjectOne/view.js
+++ ProjectOne/view.js
@@ -2,45 +2,77 @@ let kitchenInput = document.getElementById("kitchen-input");
 let addBtn = document.getElementById("add-btn");
 let kitchenItemsList = document.getElementById("kitchen-items-list");
 
-let emptyArray = [];
+
+//declaring this vraiable as globally
+let kitchenInputValue;
+
+let kitchenInputArray = [];
+
+
+
+function setLocalStorage(){
+    localStorage.setItem("kitchenItem",JSON.stringify(kitchenInputArray));
+}
+function getLocalStorage(){
+    if (localStorage.getItem("kitchenItem")){
+        kitchenInputArray = JSON.parse(localStorage.getItem("kitchenItem"));
+        buildUI()
+        console.log(kitchenInputArray);
+    } 
+    else {
+        console.log("Couldn't find the value");
+    }
+}
+
+function buildUI(){
+    kitchenItemsList.textContent = " ";
+    //to get the value one by one we should use foreach
+    kitchenInputArray.forEach((item) => {    
+
+     //DOM Elemnt Creation
+     let li = document.createElement("li");
+     // li.innerText = kitchenInputValue; its an error!!!!!!!!!
+ 
+     //create a span elemnet and appnd it into the li element 
+     let spanEl = document.createElement("span"); 
+     li.appendChild(spanEl);
+     spanEl.innerText =item;
+ 
+     //this is how we give animation style in js
+     li.style.cssText = "animation-name: slideIn;"
+ 
+     kitchenItemsList.appendChild(li); 
+     kitchenInput.value =" ";//to reset the inputbox
+     kitchenInput.focus();//focus is an inbuiod function to set the curser automatically in inputbox.
+ 
+     //creating trash button
+     let trashButton = document.createElement("i");
+     trashButton.classList.add("fas","fa-trash");
+     li.appendChild(trashButton);
+ 
+     //creating edit button
+     let editBtn = document.createElement("i");
+     editBtn.classList.add("fas","fa-edit");
+     li.appendChild(editBtn);
+    }); 
+}
 
 
 //step 2 create function for Event:
-
-function addingItems(){
+function addingItems(event){
     
-    let kitchenInputValue = kitchenInput.value;
-      
-    //array push
-    emptyArray.push(kitchenInputValue);
+    kitchenInputValue = kitchenInput.value;
 
-    //DOM Elemnt Creation
-    let li = document.createElement("li");
-    // li.innerText = kitchenInputValue; its an error!!!!!!!!!
+     //array push----> 
+     kitchenInputArray.push(kitchenInputValue);
 
-    //create a span elemnet and appnd it into the li element 
-    let spanEl = document.createElement("span"); 
-    li.appendChild(spanEl);
-    spanEl.innerText =kitchenInputValue;
+     setLocalStorage();
 
-    //this is how we give animation style in js
-    li.style.cssText = "animation-name: slideIn;"
+     getLocalStorage();
+    
+   
 
-    kitchenItemsList.appendChild(li); 
-    kitchenInput.value =" ";//to reset the inputbox
-    kitchenInput.focus();//focus is an inbuiod function to set the curser automatically in inputbox.
-    console.log(emptyArray);
-
-    //creating trash button
-    let trashButton = document.createElement("i");
-    trashButton.classList.add("fas","fa-trash");
-    li.appendChild(trashButton);
-
-    //creating edit button
-    let editBtn = document.createElement("i");
-    editBtn.classList.add("fas","fa-edit");
-    li.appendChild(editBtn);
-
+   
 }
 
 //delete item
@@ -76,3 +108,5 @@ function editButton(event) {
 addBtn.addEventListener("click", addingItems);
 kitchenItemsList.addEventListener("click", deleteItems);
 kitchenItemsList.addEventListener("click", editButton);
+
+getLocalStorage();
